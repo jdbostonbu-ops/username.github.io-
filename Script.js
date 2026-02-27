c681471a98ce516fbebc0103a479bc0ef88a05ff
jdbostonbu-ops <jdboston.bu@gmail.com>
2026-02-27 17:20:21 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,4 @@
+function changeHeading() {
+    const myHeading = document.querySelector("h1");
+    myHeading.textContent = "Making good choices with Medicare involves understanding your options!";
+}
